2de34c6c3b6c875a54f4ff5a3211c6678115aec2
tekim <rldod526@naver.com>
2023-02-27 15:54:33 +0900

    launchScreen

--- DietHelper/Scenes/LaunchScene/LaunchScreenView.swift
+++ DietHelper/Scenes/LaunchScene/LaunchScreenView.swift
@@ -6,11 +6,24 @@
 //
 
 import UIKit
+import SnapKit
 
 class LaunchScreenView: BaseView {
-    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "DIET\nHelper"
+        label.numberOfLines = 2
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 28, weight: .bold)
+        return label
+    }()
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalTo(self.snp.center)
+        }
     }
     
     required init?(coder: NSCoder) {
